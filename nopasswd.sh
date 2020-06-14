ef98bf2095b60bdb4c3d61d65a582756a5deddd2
romepeng <romepeng@outlook.com>
2020-06-14 09:43:45 +0800

    Update nopasswd.sh

--- nopasswd.sh
+++ nopasswd.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-#本机上已有 id_dsa.pub ;若无, 使用命令 ssh-keygen -t dsa romepeng@outlook.xom 获得;
+#本机上已有 id_dsa.pub ;若无, 使用命令 :
+ssh-keygen -t rsa -C "romepeng@outlook.com"
+cat ~/.ssh/id_rsa.pub
+
 # sh nopasswd.sh USER REMOTE_HOST
 scp ~/.ssh/id_rsa.pub  $1@$2:~/
 ssh $1@$2 " touch ~/.ssh/authorized_keys ; cat ~/id_rsa.pub  >> ~/.ssh/authorized_keys; chmod 644 ~/.ssh/authorized_keys; exit"
