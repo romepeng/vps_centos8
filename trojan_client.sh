97f47d900714fff0a8ce5e8224668acdc7d4181e
romepeng <romepeng@outlook.com>
2020-06-14 11:39:47 +0800

    Create trojan_client.sh

---
+++ trojan_client.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#download from https://github.com/trojan-gfw/trojan/releases
+
+proxychains4 wget https://github.com/trojan-gfw/trojan/releases/download/v1.16.0/trojan-1.16.0-linux-amd64.tar.xz
+tar -xJvf https://github.com/trojan-gfw/trojan/releases/download/v1.16.0/trojan-1.16.0-linux-amd64.tar.xz
+#cd trojan
+cp ~/trojan/example/json_client.example ~/trojan/config.json
+# vi ~/trojan/config.json
+# remote_ip--gcp_ip, passwd; ssl "verify"--fasle
+nohup ~/trojan/trojan -c ~/trojan/config.json 2>&1 &
+
+
+
