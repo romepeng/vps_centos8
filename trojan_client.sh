e6a0c45740ae344603205b91630ab0bfa458c08b
romepeng <romepeng@outlook.com>
2021-05-31 11:32:44 +0800

    Update trojan_client.sh

--- trojan_client.sh
+++ trojan_client.sh
@@ -4,7 +4,7 @@
 wget https://github.com/trojan-gfw/trojan/releases/download/v1.16.0/trojan-1.16.0-linux-amd64.tar.xz
 tar -xJvf trojan-1.16.0-linux-amd64.tar.xz
 #cd trojan
-cp ~/trojan/example/json_client.example ~/trojan/config.json
+cp ~/trojan/example/json.client-example ~/trojan/config.json
 # vi ~/trojan/config.json
 # remote_ip--gcp_ip, passwd; ssl "verify"--fasle
 nohup ~/trojan/trojan -c ~/trojan/config.json 2>&1 &
