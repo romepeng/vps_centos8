009a054e3f3a739b55f54b084f497677604199d3
romepeng <romepeng@outlook.com>
2021-05-31 11:25:40 +0800

    Update trojan_client.sh

--- trojan_client.sh
+++ trojan_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #download from https://github.com/trojan-gfw/trojan/releases
 
-proxychains4 wget https://github.com/trojan-gfw/trojan/releases/download/v1.16.0/trojan-1.16.0-linux-amd64.tar.xz
+wget https://github.com/trojan-gfw/trojan/releases/download/v1.16.0/trojan-1.16.0-linux-amd64.tar.xz
 tar -xJvf trojan-1.16.0-linux-amd64.tar.xz
 #cd trojan
 cp ~/trojan/example/json_client.example ~/trojan/config.json
